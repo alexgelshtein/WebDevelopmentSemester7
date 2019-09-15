1aae7c6b668d002eb144366a3e58b6110178d9f9
Alexander Gelshtein <agelshtein1998@gmail.com>
2019-09-15 23:01:50 +0300

    First commit

---
+++ WebDev1/scripts.js
@@ -0,0 +1,12 @@
+async function get_weather() {
+    let cityName = document.getElementById('search').value;
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=56f83e11e081b27c7005321a05b8af02`)
+    let json = await response.json();
+    console.log(json);
+    document.getElementById('1').innerHTML = json.main.temp + " °C";
+    document.getElementById('2').innerHTML = json.clouds.all + " %";
+    document.getElementById('3').innerHTML = json.main.humidity + " %";
+    document.getElementById('4').innerHTML = json.main.pressure + " mm Hg";
+    document.getElementById('5').innerHTML = json.wind.speed + " m/s";
+}
+document.getElementById('find').addEventListener('click', get_weather);
