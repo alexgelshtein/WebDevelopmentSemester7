4dd625ed64b86a522436236d764b065437708045
Alexander Gelshtein <agelshtein1998@gmail.com>
2019-09-25 09:50:00 +0300

    Some fixes

--- WebDev1/scripts.js
+++ WebDev1/scripts.js
@@ -6,11 +6,11 @@ async function get_weather() {
   if (response.ok) {
     let json = await response.json();
     console.log(json);
-    document.getElementById("1").innerHTML = json.main.temp + " °C";
-    document.getElementById("2").innerHTML = json.weather["0"].description;
-    document.getElementById("3").innerHTML = json.main.humidity + " %";
-    document.getElementById("4").innerHTML = json.main.pressure + " mm Hg";
-    document.getElementById("5").innerHTML = json.wind.speed + " m/s";
+    document.getElementById("temp").innerHTML = "Temperature:" + "</br></br>" + json.main.temp + " °C";
+    document.getElementById("desc").innerHTML = "Description:" + "</br></br>" + json.weather["0"].description;
+    document.getElementById("humid").innerHTML = "Humidity:" + "</br></br>" + json.main.humidity + " %";
+    document.getElementById("pres").innerHTML = "Pressure:" + "</br></br>" + json.main.pressure + " hPa";
+    document.getElementById("wind").innerHTML = "Wind speed:" + "</br></br>" + json.wind.speed + " m/s";
   } else {
     alert("No city found :-(\n\nPlease try again.");
   }
