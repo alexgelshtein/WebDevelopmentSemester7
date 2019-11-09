809ed12283040f76725a5479f02b7cbbded283e0
Alexander Gelshtein <agelshtein1998@gmail.com>
2019-11-09 13:49:05 +0300

    Divided main function into three separate ones

--- WebDevLab1/script.js
+++ WebDevLab1/script.js
@@ -1,38 +1,43 @@
-var enteredCity;
-
 window.onload = function () {
     document.getElementById("form1")
         .addEventListener("submit", function (event) {
-            enteredCity = event.target[0].value;
+            let enteredCity = event.target[0].value;
             event.preventDefault();
-            getWeather();
+            let requestForecast = getWeather(enteredCity);
+            handleErrors(requestForecast);
         });
 }
 
-function getWeather() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + enteredCity + '&appid=56f83e11e081b27c7005321a05b8af02', false);
+function getWeather(cityName) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=56f83e11e081b27c7005321a05b8af02', false);
     xhr.send();
-    if (xhr.status == 404) {
-        var pagefnerror = doT.template(document.getElementById('errortmpl').text, undefined);
-        var error = {
+    return xhr;
+}
+
+function handleErrors(xhr) {
+    if (xhr.status === 404 || xhr.status === 500) {
+        let pagefnerror = doT.template(document.getElementById('errortmpl').text);
+        let error = {
             text: xhr.statusText,
             code: xhr.status
         }
         document.getElementById('content').innerHTML = pagefnerror(error);
     } else {
-        var response = JSON.parse(xhr.responseText);
-        var data = {
+        parse(xhr);
+    }
+}
+
+function parse(xhr) {
+    let response = JSON.parse(xhr.responseText);
+        let data = {
             city: response.name,
             temp: (response.main.temp - 273.15).toFixed(2),
             windSpeed: response.wind.speed,
             sky: response.weather[0].description,
-            pressure: response.main.pressure,
+            pressure: (response.main.pressure * 0.75).toFixed(1),
             humidity: response.main.humidity,
         }
-        var pagefn = doT.template(document.getElementById('pagetmpl').text, undefined);
-        ///Для чего нужно undefined
+        let pagefn = doT.template(document.getElementById('pagetmpl').text);
         document.getElementById('content').innerHTML = pagefn(data);
-    }
 }
-///Разбить функцию getWeather на несколько
