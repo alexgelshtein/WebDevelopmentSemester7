f28b8fdbc35ba02ba918fabcbd357ad1cea59dde
alexgelshtein <agelshtein1998@gmail.com>
2020-02-06 22:07:38 +0300

    lab4

---
+++ web-dev-lab4/back/config/index.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+    port : 4000,
+    apiUrl : "https://api.openweathermap.org/data/2.5/weather?units=metric",
+    token: "56f83e11e081b27c7005321a05b8af02",
+    dbConnectionString: "mongodb+srv://everybody:pass@lab5-pgazr.mongodb.net/test?retryWrites=true&w=majority",
+    db : "lab4",
+    collection : "favoriteCities",
+};
